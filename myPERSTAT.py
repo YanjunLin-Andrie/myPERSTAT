20126efadfb963b4bbd18dbcdaecb7a980c23356
Yanjun Lin Andrie <98440287+YanjunLin-Andrie@users.noreply.github.com>
2022-03-28 07:37:19 -0700

    Present count displays correctly, Reasons counts display 0s

--- myPERSTAT.py
+++ myPERSTAT.py
@@ -51,8 +51,13 @@ lblNP.grid(row=0,column=3, sticky=W)
 
 # Reset button 
 def Reset():
-    check_box.deselect()
+    for i in store_var:
+        i.get() == False
+        present_count == 0
+    
     box.current(0)
+
+
 
 btnReset = Button(myframe, text='Reset', padx=2, pady=2, bd=5, fg='black', font = ('arial', 10, 'bold'),
              width = 11, height=1, command= Reset).grid(row = 0, column =4)
@@ -61,10 +66,19 @@ btnReset = Button(myframe, text='Reset', padx=2, pady=2, bd=5, fg='black', font 
 lblDate= Label(myframe, font=('arial',11,'bold'), textvariable=Date, padx=4,pady=4,bd=4,fg='black', bg = 'white', relief = 'sunken')
 lblDate.grid(row=0, column=5, sticky=W)
 
+
+# get all the counts of presence, 
+present_count = 0 
+def Present_counts(present_count=present_count):
+    for i in store_var:
+        if i.get():
+            present_count += 1
+    lbltotal_present_count['text'] = [present_count]
+
 # Loop around the list of soldiers/student/workers that needs to track presence of to build the main body of the app to track each 
 # person's status
 boxes = []
-
+store_var = []
 for i in roster:
     lblNo = Label(myframe, font=('arial',10,'bold'), text=1+roster.index(i), bd=5)
     lblNo.grid(row=1+roster.index(i), column=0, sticky=W)
@@ -72,9 +86,9 @@ for i in roster:
     lblName = Label(myframe, font = ('arial', 10,'bold'),text=i, padx=3, pady=1, bd=1, width = 12)
     lblName.grid(row=1+roster.index(i),column=1, sticky = W)
 
-    store_var = []
+    
     var = tk.IntVar()
-    check_box=ttk.Checkbutton(myframe, text = "P", variable = var)
+    check_box=ttk.Checkbutton(myframe, text = "P", variable = var, command=Present_counts)
     check_box.grid(row=1+roster.index(i), column = 2)
     store_var.append(var)
 
@@ -91,51 +105,42 @@ for i in roster:
     lblempty2 = Label(myframe,font=('arial',10,'bold'), text="", padx=1, pady=1, bd=1, fg="black", width = 12)
     lblempty2.grid(row=1+roster.index(i),column=5, sticky=W)
 
-# get all the counts of presence, 
-
-def Present_counts():
-#count how many are present from the check box   
-    present_count = 0    
-    for i in store_var:
-        if i.get():
-            present_count += 1
-    return present_count
-
-
-
 #count each occurance of each reasons from all boxes
-def Reasons_counts():
-    appointment_count = 0
-    child_care_count = 0
-    vacation_count = 0
-    for i in roster:
-        if boxes[roster.index(i)].get() == 'Appointment':
+appointment_count = 0
+child_care_count = 0
+vacation_count = 0
+def Reasons_counts(appointment_count = appointment_count, child_care_count = child_care_count, vacation_count = vacation_count):
+    for box in boxes:
+        if box.get() == 'Appointment':
             appointment_count += 1
-        if boxes[roster.index(i)].get() == 'Child Care':
+        if box.get() == 'Child Care':
             child_care_count += 1
-        if boxes[roster.index(i)].get() == 'Vacation':
+        if box.get() == 'Vacation':
             vacation_count += 1
-    return appointment_count, child_care_count, vacation_count
+    
+    lbltotal_appointment_count['text'] = [appointment_count]
+    lbltotal_child_care_count['text'] = [child_care_count]
+    lbltotal_vacation_count['text'] = [vacation_count]
 
 # last rows of the table which summarizes the total number of presence and total number of people for each reason of absence
 lbltotal_present = Label(myframe,font=('arial',14,'bold'), text="Total Present", bd=5)
 lbltotal_present.grid(row=len(roster)+2,column=1, sticky=W)
-lbltotal_present_count = Label(myframe,font=('arial',14,'bold'), command = Present_counts, bd=5)
+lbltotal_present_count = Label(myframe,font=('arial',14,'bold'), bd=5, text = present_count)
 lbltotal_present_count.grid(row=len(roster)+2,column=2, sticky=W)
 
 lbltotal_appointment = Label(myframe,font=('arial',14,'bold'), text="Total Appointment", bd=5)
 lbltotal_appointment.grid(row=len(roster)+3,column=1, sticky=W)
-lbltotal_appointment_count = Label(myframe,font=('arial',14,'bold'), bd=5)
+lbltotal_appointment_count = Label(myframe,font=('arial',14,'bold'), bd=5, text = appointment_count)
 lbltotal_appointment_count.grid(row=len(roster)+3,column=2, sticky=W)
 
 lbltotal_child_care = Label(myframe,font=('arial',14,'bold'), text="Total Child Care", bd=5)
 lbltotal_child_care.grid(row=len(roster)+4,column=1, sticky=W)
-lbltotal_child_care_count = Label(myframe,font=('arial',14,'bold'), bd=5)
+lbltotal_child_care_count = Label(myframe,font=('arial',14,'bold'), bd=5, text = child_care_count)
 lbltotal_child_care_count.grid(row=len(roster)+4,column=2, sticky=W)
 
 lbltotal_vacation = Label(myframe,font=('arial',14,'bold'), text="Total Vacation", bd=5)
 lbltotal_vacation.grid(row=len(roster)+5,column=1, sticky=W)
-lbltotal_vacation_count = Label(myframe,font=('arial',14,'bold'), bd=5)
+lbltotal_vacation_count = Label(myframe,font=('arial',14,'bold'), bd=5, text = vacation_count)
 lbltotal_vacation_count.grid(row=len(roster)+5,column=2, sticky=W)
 
 
